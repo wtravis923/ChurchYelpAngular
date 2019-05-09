462fe329cee64ad011997a9b7fd8dfaed0a1a607
wtravis923 <willie.travis@gmail.com>
2019-05-09 14:04:02 -0400

    built login page

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, FormControl} from '@angular/forms';
+import { AuthService } from '../../services/auth.service'
+
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
@@ -9,19 +11,24 @@ export class RegistrationComponent implements OnInit {
 
   private _registerForm: FormGroup;
 
-  constructor() { }
+  constructor(private _form: FormBuilder, private _authService: AuthService) { this.createForm() }
 
   ngOnInit() {
   }
+
   createForm() {
-    this._registerForm = this._registerForm.group({
+    this._registerForm = this._form.group({
       email: new FormControl,
       password: new FormControl,
       confirmPassword: new FormControl 
-    })
+    });
   }
+
   onSubmit() {
     console.log(this._registerForm.value);
+    this._authService
+    .register(this._registerForm.value)
+    .subscribe( () => this._authService.login(this._registerForm.value));
   }
 
 }
