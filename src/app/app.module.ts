ccf91c40c3204f6009b7fad1aa444026ecb3e59b
Spriggs01 <sarah.spriggs@gamil.com>
2019-05-09 09:56:21 -0400

    Registration updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from'@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { MatToolbarModule,
+MatButtonModule,
+MatFormFieldModule,
+MatInputModule
+} from '@angular/material';
 import { AppComponent } from './app.component';
-
+import { RegistrationComponent } from './components/registration/registration.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
