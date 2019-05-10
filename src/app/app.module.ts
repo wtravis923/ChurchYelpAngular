60ae1b1ce8af50893260fb4ae976969911655c99
Spriggs01 <sarah.spriggs@gamil.com>
2019-05-10 10:46:19 -0400

    updated Cors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from'@angular/forms';
@@ -14,7 +16,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { AuthService } from './services/auth.service';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { ChurchService } from './services/church.service';
 import { ChurchIndexComponent } from './components/church/church-index/church-index.component';
 import { Button } from 'protractor';
