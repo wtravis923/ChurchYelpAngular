3cb7d655683d5f3908f17a9357e4bf8393176d26
wtravis923 <willie.travis@gmail.com>
2019-05-14 09:59:06 -0400

    ChurchRating CRUD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { AuthService } from './services/auth.service';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { ChurchService } from './services/church.service';
+import { ChurchRatingsService} from './services/ChurchRatings.service';
 import { HomeComponent } from './components/home/home.component';
 import { LeaderService } from './services/leader.service';
 import { LeaderListItemComponent } from './components/leader/leader-listItem/leader-listItem.component';
@@ -37,7 +38,7 @@ const routes = [
   { path: 'church/:id', component: ChurchDeleteComponent},
   { path: 'church/edit/:id', component: ChurchEditComponent},
   { path: '**', component: HomeComponent},
-  {path: 'leader/create', component:LeaderCreateComponent},
+  { path: 'leader/create', component:LeaderCreateComponent},
 ];
 
 @NgModule({
@@ -74,8 +75,10 @@ const routes = [
   providers: [
     AuthService,
     ChurchService,
+    ChurchRatingsService,
     LeaderService,
     ChurchDetailComponent
+
   ],
 
   bootstrap: [AppComponent]
