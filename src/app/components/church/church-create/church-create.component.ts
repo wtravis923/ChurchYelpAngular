58faa0ea9d3a1d04b63107fb57868f588e7d1774
wtravis923 <willie.travis@gmail.com>
2019-05-16 14:58:00 -0400

    Leader branch update

--- src/app/components/church/church-create/church-create.component.ts
+++ src/app/components/church/church-create/church-create.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ChurchService } from '../../../services/church.service';
-import {FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,25 +9,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./church-create.component.css']
 })
 export class ChurchCreateComponent implements OnInit {
-  
+
   churchForm: FormGroup;
 
-  constructor(private _churchService: ChurchService, private _form: FormBuilder, private _router: Router ) {
+  constructor(private _churchService: ChurchService, private _form: FormBuilder, private _router: Router) {
     this.createForm();
-   }
+  }
 
   ngOnInit() {
   }
-createForm() {
-  this.churchForm = this._form.group({
-    ChurchName: new FormControl,
-    ChurchCity: new FormControl,
-    ChurchState: new FormControl
-  });
-}
-onSubmit() {
-  this._churchService.createChurch(this.churchForm.value).subscribe(data => {
-    this._router.navigate(['/church']);
-  });
-}
+  
+  createForm() {
+    this.churchForm = this._form.group({
+      ChurchName: new FormControl,
+      ChurchCity: new FormControl,
+      ChurchState: new FormControl
+    });
+  }
+  onSubmit() {
+    this._churchService.createChurch(this.churchForm.value).subscribe(data => {
+      this._router.navigate(['/church']);
+    });
+  }
 }
--- src/app/components/leader/leader-create/leader-create.component.ts
+++ src/app/components/leader/leader-create/leader-create.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { LeaderService} from '../../../services/leader.service';
-import {FormBuilder, FormGroup, FormControl} from '@angular/forms';
-import {Router} from '@angular/router';
+import { LeaderService } from '../../../services/leader.service';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -12,28 +12,23 @@ import {Router} from '@angular/router';
 export class LeaderCreateComponent implements OnInit {
 
   leaderForm: FormGroup;
+
   constructor(private _leaderService: LeaderService, private _form: FormBuilder, private _router: Router) {
     this.createForm();
-   }
+  }
 
   ngOnInit() {
   }
 
-  createForm () {
-   this.leaderForm = this._form.group ({
-     LeaderID: new FormControl,
-    LeaderName: new FormControl,
-    SpeakingAbilityRating: new FormControl,
-    EngagingRating: new FormControl,
-    AuthenticRating: new FormControl,
-    RapportRating: new FormControl
-   });
-    
+  createForm() {
+    this.leaderForm = this._form.group({
+      LeaderName: new FormControl
+    });
   }
 
   onSubmit() {
-    this._leaderService.createLeader(this.leaderForm.value).subscribe(data => {
-      this._router.navigate(['/leader']);
+    this._leaderService.createLeader (this.leaderForm.value).subscribe(data => {
+      this._router.navigate(['/Leader']);
     });
   }
 
