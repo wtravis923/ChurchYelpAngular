ee12096ae83cb4ef8afe0e587645f4dcff54838c
Matthew Patterson <mdpatterson924@gmail.com>
2019-05-17 09:20:00 -0400

    dsafdds

--- src/app/components/churchRating/church-rating-create/church-rating-create.component.ts
+++ src/app/components/churchRating/church-rating-create/church-rating-create.component.ts
@@ -34,3 +34,17 @@ export class ChurchRatingCreateComponent implements OnInit {
     });
   }
 }
+
+export class SliderFormattingExample {
+  formatLabel(value: number | null) {
+    if (!value) {
+      return 0;
+    }
+
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    }
+
+    return value;
+  }
+}
