f56472a3b2c64fc3849e047c905dbeeb1eb9c319
wtravis923 <willie.travis@gmail.com>
2019-05-21 15:09:01 -0400

    update:

--- src/app/models/leader.ts
+++ src/app/models/leader.ts
@@ -1,9 +1,5 @@
 export interface Leader {
-<<<<<<< HEAD
-    LeaderId?: number;
-=======
     LeaderID: number;
->>>>>>> 866a52ad8c82a8a0670af28ff32d9761214dc486
     LeaderName: string;
     SpeakingAbilityRating?: number;
     EngagingRating?: number;
